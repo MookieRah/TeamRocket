f8fe90e7211492bbc877ba47005267eeac83058d
jason <jason@DESKTOP-NSSP6N1>
2016-08-19 12:28:03 +0200

    Got rid of garbage in controllers/views.

--- Webbsida/Controllers/HomeController.cs
+++ Webbsida/Controllers/HomeController.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webbsida.Models;
 
 namespace Webbsida.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var events = _db.Events.ToList();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(events);
         }
     }
 }
