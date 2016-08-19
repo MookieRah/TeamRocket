2d27d699dde3306fa877819fa21a876c13526c92
chris <chris@DESKTOP-MC72F6A>
2016-08-19 13:53:13 +0200

    Fixing Event View

---
+++ Webbsida/Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.Design;
+using DatabaseObjects;
+
+namespace Webbsida.Controllers
+{
+    public class EventController : Controller
+    {
+        public EventController()
+        {
+        }
+
+        // GET: Event
+        public ActionResult Index()
+        {
+            var entreas = new Event();
+            return View(entreas);
+        }
+
+        //public ActionResult GetEvent(int id)
+        //{
+        //   Event ViewEvent = new Event();
+        //    List<ViewEvent> list = null;
+        //    try
+        //    {
+                
+        //    }
+        //    catch (Exception)
+        //    {
+                
+        //        throw;
+        //    }
+        //}
+    }
+}
