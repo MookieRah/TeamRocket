55d8a92f563cacc4ff16a27d9c65a9065e4c1016
Jimolle <jimolofsamuel@gmail.com>
2016-08-20 10:05:13 +0200

    started some geocoding

---
+++ Webbsida/Content/scripts/geoScripts.js
@@ -0,0 +1,13 @@
+﻿function showGoogleMap() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getcurrentPosition(OnSuccess, OnError, {
+            enableHighAccuracy: true,
+            maximumAge: 5000,
+            timeout: 1000
+        });
+    } else {
+        $("map").Text = "No support for geolocation!";
+    }
+
+}
