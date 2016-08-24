7cb2733c80103944e8229b4d6f74125c554f1ed9
jason <jason@DESKTOP-NSSP6N1>
2016-08-24 15:29:45 +0200

    Tacos.

--- Webbsida/App_Start/BundleConfig.cs
+++ Webbsida/App_Start/BundleConfig.cs
@@ -24,8 +24,10 @@ namespace Webbsida
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                                  "~/Content/bootstrap.css",
+                                  "~/Content/site.css",
+                                  "~/Content/custom.css"));
+            
             //Create bundel for jQueryUI  
             //js  
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
