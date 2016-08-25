d08051c1c6671a1cd7e207be9cc6f83d96bb2de5
jimolle <jimolle@users.noreply.github.com>
2016-08-25 13:33:49 +0200

    Jimolle/test project (#13)
    * started testproject
    
    * Added some tests
    
    * resetted Event.cs

---
+++ Tests/EventTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Webbsida.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class EventTests
+    {
+        private readonly ApplicationDbContext db;
+
+        public EventTests()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        [Test]
+        public void GetEventByEventName()
+        {
+            // ARRANGE
+            var expectedResult = "Name1";
+
+            // ACT
+            var result = db.Events.SingleOrDefault(n => n.Name == "Name1");
+
+            // ASSERT
+            Assert.AreEqual(expectedResult, result.Name);
+        }
+
+        [Test]
+        public void AssertThatAllEventsHasAnOwner()
+        {
+            // ARRANGE
+
+            // ACT
+            var numberOfOwnedEvents = db.EventUsers.Count(n => n.IsOwner);
+            var numberOfEventsWithOwner = db.EventUsers.Where(x => x.IsOwner).Select(x => x.Profile).Count();
+
+            // ASSERT
+            Assert.AreEqual(numberOfOwnedEvents, numberOfEventsWithOwner);
+        }
+    }
+}
