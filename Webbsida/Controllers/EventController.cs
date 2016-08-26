930741267af273ed4629c80088c938bdffe8451e
Torbjörn Boström <torbjorn_bostrom@yahoo.se>
2016-08-26 13:30:56 +0200

    File upload changed

--- Webbsida/Controllers/EventController.cs
+++ Webbsida/Controllers/EventController.cs
@@ -31,41 +31,49 @@ namespace Webbsida.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(CreateEventViewModel ev)
         {
-            // TODO: Model-Validation (and optional file uploaded)!
 
-            // TODO: Make sure this path will be correct in the db!!
-            var path = Path.Combine(Server.MapPath("/Content/EventImages/"), ev.Image.FileName);
-            var FileExtension = Path.GetExtension(ev.Image.FileName).ToLower();
-            if (FileExtension==".png" || FileExtension == ".jpg" || FileExtension == ".gif" || FileExtension == ".jpeg" || FileExtension == ".jpe" || FileExtension == ".jfif")
-            { 
-                ev.Image.SaveAs(path);
+            // TODO: Model-Validation (and optional file uploaded)!
+            if (ev.Image != null)
+            {
+                // TODO: Make sure this path will be correct in the db!!
+                var path = Path.Combine(Server.MapPath("/Content/EventImages/"), ev.Image.FileName);
+                var FileExtension = Path.GetExtension(ev.Image.FileName).ToLower();
+                if (FileExtension == ".png" || FileExtension == ".jpg" || FileExtension == ".gif" || FileExtension == ".jpeg" || FileExtension == ".jpe" || FileExtension == ".jfif")
+                {
+                    ev.Image.SaveAs(path);
+                }
+                else
+                {
+                    // return JavaScript(alert("We don't accept your filetype"));
+                    return Content("<script language='javascript' type='text/javascript'>alert('We dont accept your filetype. The filetype we ccept is .PNG, .GIF, .JPG.');</script>");
+                }
+
+                // TODO: Connect with path instead.
+                string pathToSaveInDb = @"\Content\EventImages\" + ev.Image.FileName;
+                // Shall update filetype
+
+                var res = new Event()
+                {
+                    Name = ev.Name,
+                    Description = ev.Description,
+                    StartDate = ev.StartDate,
+                    EndDate = ev.EndDate,
+                    MinSignups = ev.MinSignups,
+                    MaxSignups = ev.MaxSignups,
+                    Price = ev.Price,
+                    Latitude = ev.Latitude,
+                    Longitude = ev.Longitude,
+
+                    ImagePath = pathToSaveInDb
+                };
+                db.Events.Add(res);
+                db.SaveChanges();
             }
             else
             {
-                // return JavaScript(alert("We don't accept your filetype"));
-                return Content("<script language='javascript' type='text/javascript'>alert('We dont accept your filetype In this time we only accepting .png');</script>");
+                return Content("<script language='javascript' type='text/javascript'>alert('You need upload a file');</script>");
             }
-            // TODO: Connect with path instead.
-            string pathToSaveInDb = @"\Content\EventImages\" + ev.Image.FileName;
-            // Shall update filetype
-
-            var res = new Event()
-            {
-                Name = ev.Name,
-                Description = ev.Description,
-                StartDate = ev.StartDate,
-                EndDate = ev.EndDate,
-                MinSignups = ev.MinSignups,
-                MaxSignups = ev.MaxSignups,
-                Price = ev.Price,
-                Latitude = ev.Latitude,
-                Longitude = ev.Longitude,
-
-                ImagePath = pathToSaveInDb
-            };
-
-            db.Events.Add(res);
-            db.SaveChanges();
+                
 
             return RedirectToAction("Index");
         }
