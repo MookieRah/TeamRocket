4953c1c1defa05f68a37824737a244cc904ab161
jason <jason@DESKTOP-NSSP6N1>
2016-08-26 13:28:44 +0200

    Added new bundle to hold all of our custom js and it is rendered in _Layout

--- Webbsida/App_Start/BundleConfig.cs
+++ Webbsida/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Webbsida
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                "~/Scripts/site/geoScriptsV2.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
