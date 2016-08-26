549acd7013a26ca70255f62186e936d28ce02f39
Jason Smith <mookierah@gmail.com>
2016-08-26 10:23:18 +0200

    Had to do some derp to fix NUnit's reference in the Test assembly.

--- Tests/EventTests.cs
+++ Tests/EventTests.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Linq;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
 using Webbsida.Models;
+using NUnit.Framework;
 
 namespace Tests
 {
