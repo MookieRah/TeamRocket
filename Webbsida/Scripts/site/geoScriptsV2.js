db2b87dab365333c3e238c0b90012825bea9c70e
Jimolle <jimolofsamuel@gmail.com>
2016-08-29 17:33:33 +0200

    Major javascript bug fixed (no longer runs all functions on all pages)

--- Webbsida/Scripts/site/geoScriptsV2.js
+++ Webbsida/Scripts/site/geoScriptsV2.js
@@ -6,8 +6,8 @@ var GoogleMapsEventController = function () {
     var geocoder;
     var userMarker = false;
 
-    // Use geolocation to try to fetch user location, if successful; invoke "onSuccess" else "onError"
-    function getUserPosition() {
+    // 1. Use geolocation to try to fetch user location, if successful; invoke "onSuccess" else "onError"
+    var getUserPosition = function() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(onSuccess, onError, {
                 //Default values used for now here:
@@ -19,7 +19,7 @@ var GoogleMapsEventController = function () {
         } else {
             document.getElementById("map").innerHTML = "GeoLocation not supported";
         }
-    }
+    };
 
     function onSuccess(position) {
         setUserLocation(position.coords.latitude, position.coords.longitude);
@@ -53,7 +53,7 @@ var GoogleMapsEventController = function () {
 
 
     // Three different main entry points when you want to use a map:
-    // 1. Start with a map displaying ALL events
+    // 2. Start with a map displaying ALL events
     // TODO: Just list active events?
     var initAllEventsDisplayingMap = function () {
 
@@ -85,7 +85,7 @@ var GoogleMapsEventController = function () {
     };
 
 
-    // 2. Map displaying single event
+    // 3. Map displaying single event
     var initSingleEventDisplayingMap = function () {
 
         var eventId = $("#Id").val();
@@ -130,7 +130,7 @@ var GoogleMapsEventController = function () {
         );
     }
 
-    // 3. Start with a pickermap
+    // 4. Start with a pickermap
     var initPickerMap = function () {
 
         getUserPosition();
@@ -165,10 +165,9 @@ var GoogleMapsEventController = function () {
     };
 
 
-    // 4. Distance calculations
-    // When you get your results, start looping trough them and calculate distances,
-    // return a sorted "dictionary"!
-    var getEventsWithDistance = new function () {
+    // 5. Distance calculations
+    // Not used atm.
+    var getEventsWithDistance = function () {
 
         // TODO: If this fails return!
         getUserPosition();
@@ -185,14 +184,14 @@ var GoogleMapsEventController = function () {
         });
 
     };
-    // END DISTANCE
 
 
-    function setMarkerByUserInputAddress() {
+    // 6.
+    var setMarkerByUserInputAddress = function() {
 
         geocoder = new google.maps.Geocoder();
 
-        geocoder.geocode({ "address": $("#input_address").val() }, function (results, status) {
+        geocoder.geocode({ "address": $("#input_address").val() }, function(results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
                 map.setCenter(results[0].geometry.location);
 
@@ -204,7 +203,7 @@ var GoogleMapsEventController = function () {
                         draggable: true //make it draggable
                     });
                     //Listen for drag events!
-                    google.maps.event.addListener(userMarker, "dragend", function (event) {
+                    google.maps.event.addListener(userMarker, "dragend", function(event) {
                         markerLocation();
                     });
                 } else {
@@ -212,13 +211,12 @@ var GoogleMapsEventController = function () {
                 }
 
                 markerLocation();
-            }
-            else {
+            } else {
                 alert("Geocode was not successful for the following reason: " + status);
             }
         });
 
-    }
+    };
 
     function markerLocation() {
         //Get location.
@@ -241,137 +239,8 @@ var GoogleMapsEventController = function () {
         initAllEventsDisplayingMap: initAllEventsDisplayingMap,
         initSingleEventDisplayingMap: initSingleEventDisplayingMap,
         initPickerMap: initPickerMap,
-        setMarkerByUserInputAddress: setMarkerByUserInputAddress,
-        getEventsWithDistance: getEventsWithDistance
+        getEventsWithDistance: getEventsWithDistance,
+        setMarkerByUserInputAddress: setMarkerByUserInputAddress
     };
 
 }();
-
-
-
-//
-// -> Old GeoPicker stuff not used
-//
-
-//function getAddressFromCoordinates() {
-
-//    if (geocoder == null) {
-//        geocoder = new google.maps.Geocoder();
-//    }
-
-//    var input = $("#lat").val() + "," + $("#lng").val();
-//    var latlngStr = input.split(',', 2);
-//    var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1]) };
-
-
-//    geocoder.geocode({ 'location': latlng }, function (results, status) {
-//        if (status === 'OK') {
-//            if (results[1]) {
-//                alert(results[0].formatted_address);
-//            } else {
-//                window.alert('No results found');
-//            }
-//        } else {
-//            window.alert('Geocoder failed due to: ' + status);
-//        }
-//    });
-
-//}
-
-//// Request coordinates from address in a form-field
-//// The insane way (extra roundtrip to the server)
-//function requestCoordinatesFromAddress() {
-//    var getAddressFormValue = $("#input_address").val();
-
-//    $.get("RequestCoordinatesFromAddress", "address=" + getAddressFormValue, function (data) {
-//        alert(data.Latitude + ":" + data.Longitude);
-//    });
-
-//}
-
-//// Server-side = wild roundtrip, DONT USE
-//function getAddressFromCoordinatesSERVERSIDE() {
-//    //debugger;
-//    var lat = $("#lat").val();
-//    var lon = $("#lng").val();
-
-
-//    $.get("RequestAddressFromCoordinates", "lat=" + lat +"&lon=" + lon, function (data) {
-//        alert(data.addressResult);
-//    });
-//}
-
-//// 4. Distance calculations ###BACKUP
-//// When you get your results, start looping trough them and calculate distances,
-//// return a sorted dictionary!
-//var getEventsWithDistanceBACKUP = new function () {
-
-//    // TODO: If this fails return!
-//    getUserPosition();
-
-//    //var queryString = "lat=" + localStorage.getItem("pos_lat") + "?long=" + localStorage.getItem("pos_long");
-//    var result = [];
-//    $.get("/api/GeoData", function (data) {
-
-//        $.each(data, function (id, event) {
-
-//            // TODO: Sorting value NOT CORRECT
-//            result.push({
-//                key: event.Id,
-//                value: getDistance({ lat: localStorage.getItem("pos_lat"), lng: localStorage.getItem("pos_long") },
-//                { lat: event.Latitude, lng: event.Longitude }
-//                )
-//            });
-
-//        });
-
-//        console.log(result); // original dictionary
-//        console.log(sortJsObject(result)); // sorted dict
-
-//        return sortJsObject(result);
-
-//    });
-
-//    function sortJsObject(dict) {
-
-//        var keys = [];
-//        for (var key in dict) {
-//            keys[keys.length] = key;
-//        }
-
-//        var values = [];
-//        for (var i = 0; i < keys.length; i++) {
-//            values[values.length] = dict[keys[i]];
-//        }
-
-//        var sortedValues = values.sort(sortNumber);
-//        //alert(sortedValues);
-
-//        //console.log(sortedValues);
-//        return sortedValues;
-//    }
-
-//    // this is needed to sort values as integers
-//    function sortNumber(a, b) {
-//        return a - b;
-//    }
-
-//    // used in distance calculation getDistance
-//    var rad = function (x) {
-//        return x * Math.PI / 180;
-//    };
-
-//    var getDistance = function (p1, p2) {
-//        var R = 6378137; // Earth’s mean radius in meter
-//        var dLat = rad(p2.lat - p1.lat);
-//        var dLong = rad(p2.lng - p1.lng);
-//        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-//          Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
-//          Math.sin(dLong / 2) * Math.sin(dLong / 2);
-//        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//        var d = R * c;
-//        return d; // returns the distance in meter
-//    };
-
-//};
-//// END DISTANCE###BACKUP
