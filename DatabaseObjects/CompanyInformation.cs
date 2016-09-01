e373773244816acdcb9ea4e12270c13085bd48c2
Torbjörn Boström <torbjorn_bostrom@yahoo.se>
2016-09-01 14:59:22 +0200

    Start of Contact us

---
+++ DatabaseObjects/CompanyInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseObjects
+{
+    class CompanyInformation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+        public float Longitude { get; set; }
+        public float Altitude { get; set; }
+        public string Phonenumber { get; set; }
+
+
+    }
+}
---
+++ Webbsida/Models/CompanyInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webbsida.Models
+{
+    public class CompanyInformation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Beskrivning")]
+        public string Description { get; set; }
+
+        [Display(Name = "Longitud")]
+        public float Longitude { get; set; }
+
+        [Display(Name = "Latitud")]
+        public float Altitude { get; set; }
+
+        [Display(Name = "Telefon nummer")]
+        public string Phonenumber { get; set; }
+    }
+}
