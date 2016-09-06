7acf4b8203ee3bd99c148460c5db18eb8070e9b4
victor ericsson <token_master@hotmail.com>
2016-09-06 09:19:55 +0200

    Translated my page to swedish and change to the real name of the user

--- Webbsida/ViewModels/MyEventsViewModel.cs
+++ Webbsida/ViewModels/MyEventsViewModel.cs
@@ -7,7 +7,8 @@ namespace Webbsida.ViewModels
     public class MyEventsViewModel
     {
         public string UserName { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public List<IndexEventViewModel> EventsOwned { get; set; }
         public List<IndexEventViewModel> EventsBooked { get; set; }
     }
